8ae5199ddc382e733cc6fee3be4977200fc4143b
is84092 <jacker.wu@gmail.com>
2016-04-20 14:00:36 +0800

    Update videojs.vr.js

--- videojs-demo/videojs.vr.js
+++ videojs-demo/videojs.vr.js
@@ -141,6 +141,18 @@
 
 
             }
+            
+            function fullscreen() {
+                if (container.requestFullscreen) {
+                    container.requestFullscreen();
+                } else if (container.msRequestFullscreen) {
+                    container.msRequestFullscreen();
+                } else if (container.mozRequestFullScreen) {
+                    container.mozRequestFullScreen();
+                } else if (container.webkitRequestFullscreen) {
+                    container.webkitRequestFullscreen();
+                }
+            }
 
             initScene();
 
