0742c6a2a231faeb2223a3b46c099c067ed6872b
is84092 <jacker.wu@gmail.com>
2016-04-25 11:29:09 +0800

    Added files via upload

---
+++ videojs-demo/videojs2canvas.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Jacker on 2016/4/25.
+ */
+(function () {
+    var plugin;
+
+    // create a really simple plugin
+    plugin = function (options) {
+        var player = this,
+            videoEl = this.el().getElementsByTagName('video')[0],
+            container = this.el(),
+            movieMaterial,
+            movieGeometry,
+            movieScreen,
+            scene,
+            controls;
+
+        console.log(container);
+        console.log(videoEl);
+
+        var renderedCanvas = document.createElement("canvas");
+        renderedCanvas.style.width = "inherit";
+        renderedCanvas.style.height = "inherit";
+
+        container.insertBefore(renderedCanvas, container.firstChild);
+        videoEl.style.display = "none";
+    };
+
+    // register the plugin
+    videojs.plugin('vr', plugin);
+
+})();
