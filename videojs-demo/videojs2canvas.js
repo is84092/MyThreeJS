2cd386b840e2d0434c23c8a8100dd4e9b5b0d23f
is84092 <jacker.wu@gmail.com>
2016-04-25 15:37:39 +0800

    Update videojs2canvas.js

--- videojs-demo/videojs2canvas.js
+++ videojs-demo/videojs2canvas.js
@@ -1,3 +1,4 @@
+
 /**
  * Created by Jacker on 2016/4/25.
  */
@@ -6,17 +7,13 @@
 
     // create a really simple plugin
     plugin = function (options) {
+        var togglePlay;
+        var btn = document.getElementById("play-btn");
+        btn.disabled = true;
         var player = this,
-            videoEl = this.el().getElementsByTagName('video')[0],
-            container = this.el(),
-            movieMaterial,
-            movieGeometry,
-            movieScreen,
-            scene,
-            controls;
-
-        console.log(container);
-        console.log(videoEl);
+            videoEl = this.el().getElementsByTagName('video')[0];
+        var camera, scene, renderer, geometry, texture, mesh;
+        var width = window.innerWidth, height = window.innerHeight;
 
         var renderedCanvas = document.createElement("canvas");
         renderedCanvas.style.width = "inherit";
@@ -24,10 +21,64 @@
 
         videoEl.parentElement.insertBefore(renderedCanvas, videoEl.parentElement.firstChild);
         videoEl.style.display = "none";
-        
+
+        var prms1 = new Promise(function (resolve, reject) {
+            videoEl.addEventListener('canplay', function () {
+                resolve();
+            });
+            videoEl.addEventListener('error', function () {
+                reject();
+                alert('failed loading video');
+            });
+        });
+
+
+        Promise.all([prms1]).then(function () {
+            btn.disabled = false;
+        });
+        videoEl.load();
+        renderer = new THREE.WebGLRenderer({ canvas: renderedCanvas } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, width / height, 1, 1100 );
+        camera.target = new THREE.Vector3( 0, 0, 0 );
+        scene.add(camera);
+        texture = new THREE.VideoTexture( videoEl );
+        texture.minFilter = THREE.LinearFilter;
+        texture.format = THREE.RGBFormat;
+        var material = new THREE.MeshBasicMaterial({ map: texture });
+        geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
+        geometry.scale(-1, 1, 1);
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+        (function animate() {
+            if (videoEl.readyState === videoEl.HAVE_ENOUGH_DATA) {
+                texture.needsUpdate = true;
+            }
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }());
+        togglePlay = function () {
+            console.log("test");
+            if (videoEl.paused) {
+                videoEl.play();
+                (function loop() {
+                    if (videoEl.readyState === videoEl.HAVE_ENOUGH_DATA) {
+                        texture.needsUpdate = true;
+                    }
+                    requestAnimationFrame(loop);
+                    renderer.render(scene, camera);
+                }());
+            } else {
+                videoEl.pause();
+            }
+        };
+        btn.addEventListener('click', togglePlay);
+
     };
 
     // register the plugin
     videojs.plugin('vr', plugin);
+
 
 })();
